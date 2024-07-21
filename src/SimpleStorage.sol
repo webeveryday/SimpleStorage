4050e593f62932e582989de99c6c61aa6f0739bb
webeveryday <web3everyday@proton.me>
2024-07-21 09:58:08 -0400

    Clean Up

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -2,25 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract SimpleStorage {
-  uint256 myFavoriteNumber;
+    uint256 myFavoriteNumber;
 
-  struct Person {
-    uint256 favoriteNumber;
-    string name;
-  }
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
 
-  Person[] public listOfPeople;
+    Person[] public listOfPeople;
 
-  function store(uint256 _favoriteNumber) public {
-      myFavoriteNumber = _favoriteNumber;
-  }
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
 
-  function retrieve() public view returns(uint256) {
-      return myFavoriteNumber;
-  }
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
 
-  function addPerson(string memory _name, uint256 _favoriteNumber) public {
-      Person memory newPerson = Person(_favoriteNumber, _name);
-      listOfPeople.push(newPerson);
-  }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        Person memory newPerson = Person(_favoriteNumber, _name);
+        listOfPeople.push(newPerson);
+    }
 }
